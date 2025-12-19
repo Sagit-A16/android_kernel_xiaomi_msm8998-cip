58e0beac0a1ecf75446873b55c5efe9f11e7af42
deny2311 <deny2311@gmail.com>
2025-12-19 03:21:22 +0000

    add ksu next patch

---
+++ nextpatch.sh
@@ -0,0 +1,3 @@
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s stable
+echo "Adding KernelSU manager support..."
+echo "Done adding KernelSU manager support"
