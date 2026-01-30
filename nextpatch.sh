5d03aa03879eb83c6f6425b5d1c16deee13eee6c
deny2311 <deny2311@gmail.com>
2026-01-30 04:24:37 +0000

    Revert "add ksu next patch"
    This reverts commit 58e0beac0a1ecf75446873b55c5efe9f11e7af42.

--- nextpatch.sh
+++
@@ -1,3 +0,0 @@
-curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s stable
-echo "Adding KernelSU manager support..."
-echo "Done adding KernelSU manager support"
